7018ea03d54906b31a1e9783739c7ae05f360f7c
Ian Ward <ian@excess.org>
2016-11-30 15:57:00 -0500

    utf8-bomify

---
+++ utf8-bomify.bat
@@ -0,0 +1,16 @@
+@echo off
+
+if exist "%1" (
+	echo creating %1-bom.csv...
+) else (
+    echo drag a CSV file onto this script to create
+    echo a copy of with a UTF8 BOM prepended, then
+    echo automatically open that file in Excel
+	pause
+	exit
+)
+
+set "BOM=﻿"
+<nul set /p = %BOM%> %1-bom.csv
+type %1 >> %1-bom.csv
+start %1-bom.csv
