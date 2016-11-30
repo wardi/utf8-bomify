6a0eeb995861130a72dd169760363ce2b996aa5e
Ian Ward <ian@excess.org>
2016-11-30 16:03:20 -0500

    consistent indentation

--- utf8-bomify.bat
+++ utf8-bomify.bat
@@ -1,13 +1,13 @@
 @echo off
 
 if exist "%1" (
-	echo creating %1-bom.csv...
+    echo creating %1-bom.csv...
 ) else (
     echo drag a CSV file onto this script to create
     echo a copy of with a UTF8 BOM prepended, then
     echo automatically open that file in Excel
-	pause
-	exit
+    pause
+    exit
 )
 
 set "BOM=﻿"
